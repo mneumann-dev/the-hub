5b28029de770dbda61b228be7becd95374528382
Michael Neumann <mkneumann777@icloud.com>
2020-11-07 05:45:14 -0800

    domain change

--- js/projects.js
+++ js/projects.js
@@ -4,8 +4,8 @@ const data = [
     description: "This web app displays COVID-19 data that is retrieved with the <a href='https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API' rel='noopener noreferrer' target='_blank'><code>fetch API</code></a>. It incorporates the <a href='https://fusejs.io' rel='noopener noreferrer' target='_blank'><code>fuse.js</code></a> library to search for countries. The latest version uses <a href='https://parceljs.org' rel='noopener noreferrer' target='_blank'><code>parcel.js</code></a> for bundling.",
     image: 'https://ksthehub.s3-us-west-1.amazonaws.com/img/covid-19-tracker%400.1x.jpg',
     href: {
-      git: 'https://github.com/krabfish/covid-19-tracker/blob/master/README.md',
-      site: 'https://krabfish.github.io/covid-19-tracker/'
+      git: 'https://github.com/mneumann-dev/covid-19-tracker/blob/master/README.md',
+      site: 'https://mneumann-dev.github.io/covid-19-tracker/'
     },
     tags: ['#html', '#javascript', '#css', '#api']
   },
@@ -14,7 +14,7 @@ const data = [
     description: "A landing page that utilizes standard web technologies and practices. The web page is bundled using <a href='https://parceljs.org' rel='noopener noreferrer' target='_blank'><code>parcel.js</code></a>. Development time was extremely constrained.",
     image: 'https://ksthehub.s3-us-west-1.amazonaws.com/img/giamedical-landing-page%400.1x.jpg',
     href: {
-      git: 'https://github.com/krabfish/giamedical-landing-page/blob/master/README.md',
+      git: 'https://github.com/mneumann-dev/giamedical-landing-page/blob/master/README.md',
       site: 'https://giamedical.com'
     },
     tags: ['#html', '#javascript', '#bootstrap']
@@ -26,8 +26,8 @@ const data = [
       " library - <a href='https://github.com/uuidjs/uuid'>UUID</a> - to generate unique IDs",
     image: 'https://ksthehub.s3-us-west-1.amazonaws.com/img/neu-to-dos%400.1x.jpg',
     href: {
-      git: 'https://github.com/krabfish/neu-to-dos',
-      site: 'https://krabfish.github.io/neu-to-dos/'
+      git: 'https://github.com/mneumann-dev/neu-to-dos',
+      site: 'https://mneumann-dev.github.io/neu-to-dos/'
     },
     tags: ['#webcomponents', '#bootstrap']
   },
@@ -36,8 +36,8 @@ const data = [
     description: 'The Hub is a website that is made dynamic with the use of web components and standard JavaScript.',
     image: 'https://ksthehub.s3-us-west-1.amazonaws.com/img/the-hub%400.1x.jpg',
     href: {
-      git: 'https://github.com/krabfish/the-hub',
-      site: 'https://krabfishstudios.com'
+      git: 'https://github.com/mneumann-dev/the-hub',
+      site: 'https://mneumann.me'
     },
     tags: ['#html', '#css', '#webcomponents']
   }
